1db767fcda19f6fd8c962e8710340c19ae27d2d4
shfaizan <sentry88prime@gmail.com>
2020-04-04 23:16:31 +0530

    add rest operator

--- Basic JavaScript/spread and rest.js
+++ Basic JavaScript/spread and rest.js
@@ -23,4 +23,19 @@ console.log(copiedObject);
 // (They pull out all the element or the property of array or the object)
 const copiedArray = [...hobbies]
 
-console.log(copiedArray)
+console.log(copiedArray);
+
+// This a normal use of array in a function and we can not add more than 3 values if dynamically.
+// // const toArray = (arg1,arg2,arg3) => {
+// //     return[arg1,arg2,arg3];
+// // };
+
+// To print any number of values without the restriction of declaring the arguments
+// We use rest operator 
+// rest is declare in the same way as spread ie with three dots
+// The place that we use it define what we call it.
+
+const toArray = (...args) =>{
+    return args
+};
+console.log(toArray(1,2,3,4));
