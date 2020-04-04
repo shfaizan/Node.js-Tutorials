499bb6c65ac60fce8522b700ee1c41a2123dcf93
shfaizan <sentry88prime@gmail.com>
2020-04-04 22:39:53 +0530

    we added slice operator to copy original array

--- Basic JavaScript/spread and rest.js
+++ Basic JavaScript/spread and rest.js
@@ -4,6 +4,11 @@ const hobbies = ['sports','coading'];
 // This is the nested array and not the exact copy of the real array.
 // // const copiedArray = [hobbies];
 
+// We use slice operator to copy the original array
+// //const copiedArray = hobbies.slice();
+
+// This created the exact copy of the original array.
+// This is spred operator the three dot pre array name
 const copiedArray = [...hobbies]
 
 console.log(copiedArray)
