db967b9cdfed7a004d79de03a55ad9fee9b894aa
shfaizan <sentry88prime@gmail.com>
2020-04-04 14:33:47 +0530

    edited launch.json

--- Basic JavaScript/fuction.js
+++ Basic JavaScript/fuction.js
@@ -3,7 +3,7 @@ var age = 22
 var hobbies = true
 
 function UserDetail(username,userage,userhobbies) {
-    return('User named' + username + 'of age' + userage + 'has Hobbies'+ userhobbies)
+    return('User named ' + username + ' of age ' + userage + ' has Hobbies '+ userhobbies)
 }
 
 console.log(UserDetail(name,age,hobbies))
