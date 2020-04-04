113f010c33cbe136b4a18a46930f45c96c810f00
shfaizan <sentry88prime@gmail.com>
2020-04-04 22:12:53 +0530

    formatting

--- Basic JavaScript/Sample array.js
+++ Basic JavaScript/Sample array.js
@@ -1,9 +1,12 @@
 const hobbies = ['sports','coading'];
+
 // for (let hobbie of hobbies)
 // {
 //     console.log(hobbie);
 // }
+
 // 'map' keyword modify the array without accualy adding anything to the array.
 console.log(hobbies.map(hobby => ('Hobby: ' + hobby )));
+
 // The array will not be permenantly modify 
 console.log(hobbies);
