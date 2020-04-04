d6db8a2b2f04044d9a044859fa780f88a72547ea
shfaizan <sentry88prime@gmail.com>
2020-04-04 21:28:21 +0530

    added comments and comment out the unused code

--- Basic JavaScript/object.js
+++ Basic JavaScript/object.js
@@ -1,6 +1,13 @@
 const person = {
     name : 'Faizan',
     age : 22,
+
+    
+    // This is the sample way to use the arrow function the result will gave undefinded name as the 
+    // 'this' keyword refer to the global variable. 
+    // // greet : () => {
+    // //     console.log('Hi I am '+ this.name)
+    // // }
     greet : () => {
         console.log('Hi I am '+ this.name)
     }
