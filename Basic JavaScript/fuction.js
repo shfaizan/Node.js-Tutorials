72fb0bea11e4f4eaef391d83329ec800a462362e
shfaizan <sentry88prime@gmail.com>
2020-04-04 14:16:46 +0530

    added fuction for user detail

--- Basic JavaScript/fuction.js
+++ Basic JavaScript/fuction.js
@@ -2,3 +2,8 @@ var name = "faizan"
 var age = 22
 var hobbies = true
 
+function UserDetail(username,userage,userhobbies) {
+    return('User named' + username + 'of age' + userage + 'has Hobbies'+ userhobbies)
+}
+
+console.log(UserDetail(name,age,hobbies))
