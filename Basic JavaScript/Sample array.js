ea86031ae9c50610d74ca51ca7977556beff3940
shfaizan <sentry88prime@gmail.com>
2020-04-04 21:56:38 +0530

    edited the array with the map keyword

--- Basic JavaScript/Sample array.js
+++ Basic JavaScript/Sample array.js
@@ -1,5 +1,7 @@
 const hobbies = ['sports','coading'];
-for (let hobbie of hobbies)
-{
-    console.log(hobbie);
-}
+// for (let hobbie of hobbies)
+// {
+//     console.log(hobbie);
+// }
+console.log(hobbies.map(hobby => ('Hobby: ' + hobby )));
+console.log(hobbies);
