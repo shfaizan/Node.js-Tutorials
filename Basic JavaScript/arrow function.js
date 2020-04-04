938d73c71169b2d86b8f8c5ba38e73540ef7106f
shfaizan <sentry88prime@gmail.com>
2020-04-04 17:04:25 +0530

    added a add function

--- Basic JavaScript/arrow function.js
+++ Basic JavaScript/arrow function.js
@@ -9,4 +9,9 @@ const UserDetail = (username,userage,userhobbies) => {
     return('User named ' + username + ' of age ' + userage + ' has Hobbies '+ userhobbies);
 }
 
+const add = (a,b)=> {
+    return(a+b);
+}
+
+console.log(add(2,3))
 console.log(UserDetail(name,age,hobbies))
