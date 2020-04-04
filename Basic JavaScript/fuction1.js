07a4976c269142541713f15a77bdfa97365509e5
shfaizan <sentry88prime@gmail.com>
2020-04-04 14:44:51 +0530

    created a second sample code of function in javascript

--- Basic JavaScript/fuction1.js
+++ Basic JavaScript/fuction1.js
@@ -1,6 +1,9 @@
-var name = "faizan"
-var age = 21
-var hobbies = true
+// Same as the function.js but here we use the 'let' and 'const' instead of var
+const name = "faizan"
+let age = 22
+const hobbies = true
+
+age= 23;
 
 function UserDetail(username,userage,userhobbies) {
     return('User named ' + username + ' of age ' + userage + ' has Hobbies '+ userhobbies);
