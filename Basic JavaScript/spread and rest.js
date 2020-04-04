bf440059797fffe2107c8b0bd12e4b4293249b28
shfaizan <sentry88prime@gmail.com>
2020-04-04 22:35:46 +0530

    spred operator

--- Basic JavaScript/spread and rest.js
+++ Basic JavaScript/spread and rest.js
@@ -1,1 +1,9 @@
 // Spread and rest operator
+const hobbies = ['sports','coading'];
+
+// This is the nested array and not the exact copy of the real array.
+// // const copiedArray = [hobbies];
+
+const copiedArray = [...hobbies]
+
+console.log(copiedArray)
