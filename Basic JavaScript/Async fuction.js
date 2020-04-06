fbdc65b44b6354cc3e1501eb70141ee71bf1a967
shfaizan <sentry88prime@gmail.com>
2020-04-06 12:46:03 +0530

    added some basic comments

--- Basic JavaScript/Async fuction.js
+++ Basic JavaScript/Async fuction.js
@@ -1,4 +1,14 @@
 // This is the asyncronous function
+// asyncronous code because it doesn't run Immediately
+
+// here the delay of 2000ms or 2 sec is done.
 setTimeout(() => {
     console.log("Hello after 2 sec")
-},5000)
+},2000)
+// Here we have set dalay of 1 ms 
+setTimeout(() => {
+    console.log("superfast with delay of 1ms")
+},1)
+
+console.log("without any delay")
+
