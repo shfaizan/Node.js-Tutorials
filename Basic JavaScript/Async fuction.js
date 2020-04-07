4bcdf5664ec2b17812f2ed9ffe8ae1f95b16c4ba
shfaizan <sentry88prime@gmail.com>
2020-04-07 14:19:42 +0530

    created a function call fetchdata

--- Basic JavaScript/Async fuction.js
+++ Basic JavaScript/Async fuction.js
@@ -1,10 +1,20 @@
 // This is the asyncronous function
 // asyncronous code because it doesn't run Immediately
 
-// here the delay of 2000ms or 2 sec is done.
+const fetchData = callback => {
+    setTimeout(() => {
+        callback('Done!');
+    },1500 );
+}
+
+// Here the delay of 2000ms or 2 sec is done.
 setTimeout(() => {
-    console.log("Hello after 2 sec")
-},2000)
+    console.log("Hello after 2 sec");
+    fetchData(Text => {
+        console.log(Text);
+    });
+},2000);
+
 // Here we have set dalay of 1 ms 
 setTimeout(() => {
     console.log("superfast with delay of 1ms")
