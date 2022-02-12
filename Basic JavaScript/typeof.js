e8ee67a3cd54294f81f30f823c5b5e83c55662d3
Faizan <sentry88prime@gmail.com>
2022-02-13 00:12:50 +0530

    typeof

---
+++ Basic JavaScript/typeof.js
@@ -0,0 +1,39 @@
+// Object
+// let a = {
+//     name: "fz",
+//     id: 2
+// }
+
+// number
+// let a =2
+
+//string
+// let a = "Hello"
+
+//bollean 
+// let a = true
+
+// Symbol
+//let a = Symbol("This is symbol");
+
+// Function 
+//function a(){}
+
+// Arrow Fuction type is function
+// const a = () => {}
+// let a = () => {}
+
+// Shows type as Function when it is Class
+// class a {
+//     constructor(f,l,e){
+//         this.f = f;
+//         this.l = l;
+//         this.e = e; 
+//     }
+// }
+
+// Undefined
+// let a =undefined;
+
+
+console.log(typeof a)
---
+++ tempCodeRunnerFile.javascript
@@ -0,0 +1,20 @@
+
+function a() {
+    var myvar = 1;
+    for(var i = 0; i < 10; i++){
+        var myvar = i+1;
+        console.log(myvar);
+        
+    }
+}console.log(myvar);
+
+// function b() {
+//     let mylet = 1;
+//     for (i=0; i<10; i++){
+//         let mylet = i
+//         console.log(mylet);
+//     }
+// }console.log(mylet)
+
+a()
+
